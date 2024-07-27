5e0ef8fc5ec9b2414663b03ae6e1dc0b049974e8
İlayda Karahan <ilaydapala15@gmail.com>
2024-07-27 23:40:41 +0300

    updated

---
+++ Templates/HotelProject.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,38 @@
+﻿using HotelProject.WebUI.Dtos.ContactDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace HotelProject.WebUI.Controllers
+{
+	public class ContactController : Controller
+	{
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+        [HttpGet]
+        public PartialViewResult AddContact()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddContact(CreateContactDto createContactDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            await client.PostAsync("http://localhost:5093/api/Contact", stringContent);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
